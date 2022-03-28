e5a3b76b9078b3ff4c958c7aa6d75966630abf25
Piotr <piotr.sewastianik@aptiv.com>
2022-03-28 14:56:47 +0200

    prime factors part 5

--- prime_factors.py
+++ prime_factors.py
@@ -1,2 +1,17 @@
+import math
+
+
 def prime_factors(number):
-    pass
+    result = []
+    while number % 2 == 0:
+        result.append(2)
+        number = number / 2
+
+    for i in range(3, int(math.sqrt(number)) + 1, 2):
+
+        while number % i == 0:
+            result.append(i)
+            number = number / i
+    if number > 2:
+        result.append(number)
+    return result
